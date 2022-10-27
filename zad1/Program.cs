a7de9324cb31c926750908863c4890ce1ef95803
Mischa <GerasimowMichail@yandex.ru>
2022-10-27 23:53:25 +0300

    решение задачи по сравнению чисел

--- zad1/Program.cs
+++ zad1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая на вход принимает два числа 
+//и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите  число "); //вывели значение в скобках
+int a=Convert.ToInt32 ( Console.ReadLine()); // производим конвертацию в int 32
+int b=Convert.ToInt32 ( Console.ReadLine());
+if (a>b)
+Console.WriteLine(a+ " > "+b);
+else 
+Console.WriteLine(b+ " > "+a);
