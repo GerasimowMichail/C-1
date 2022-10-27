0a64eb15138488ba8e6beae2a93433496db0c008
Mischa <GerasimowMichail@yandex.ru>
2022-10-28 00:14:52 +0300

    Выявление максимального числа из 3-х

--- zad2/Program.cs
+++ zad2/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите три числа "); //вывели значение в скобках
+int a=Convert.ToInt32 ( Console.ReadLine()); // производим конвертацию в int 32
+int b=Convert.ToInt32 ( Console.ReadLine());
+int b=Convert.ToInt32 ( Console.ReadLine());
+int max1;
+int max2;
+int max;
+int c;
+if (a>b)
+    {
+        max1 = a;
+                     
+    }
+
+else if (a<b)
+    {
+        max2 = b;                     
+    }
+if (max1 < c)
+    {
+        console.Write("Максимальное число " + с);
+    }
+else 
+    Console.Write("Максимальное число " + a);
+
+    
+
+Console.Write(b+ " > "+a);
