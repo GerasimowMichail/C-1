62d24689d8dbbbfa85904499f4a4ab60a84e4529
Mischa <GerasimowMichail@yandex.ru>
2022-10-28 00:27:54 +0300

    решение задачи с четными числами от 1 до N

--- zad4/Program.cs
+++ zad4/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите натуральное число ");
+int x=Convert.ToInt32 ( Console.ReadLine());
+
+int i = 1;
+while (i<x)
+{
+   if (i%2 = 0)
+    Console.WriteLine("четные числа {i}");
+    i=i+1;
+}
