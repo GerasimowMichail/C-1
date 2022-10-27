332314126292ddacf0af808900fa67452932b5d5
Mischa <GerasimowMichail@yandex.ru>
2022-10-27 23:55:47 +0300

    задача решена

--- zad1/Program.cs
+++ zad1/Program.cs
@@ -5,10 +5,10 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-Console.WriteLine("Введите  число "); //вывели значение в скобках
+Console.WriteLine("Введите два числа "); //вывели значение в скобках
 int a=Convert.ToInt32 ( Console.ReadLine()); // производим конвертацию в int 32
 int b=Convert.ToInt32 ( Console.ReadLine());
 if (a>b)
-Console.WriteLine(a+ " > "+b);
+Console.Write(a+ " > "+b);
 else 
-Console.WriteLine(b+ " > "+a);
+Console.Write(b+ " > "+a);
