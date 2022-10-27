5cc97e09d95a4bdb37aa275237a8cb4e0c730a5a
Mischa <GerasimowMichail@yandex.ru>
2022-10-28 01:09:20 +0300

    решение задачи по поиску максимального среди 3-х чисел

--- zad2/Program.cs
+++ zad2/Program.cs
@@ -5,11 +5,11 @@
 // 22 3 9 -> 22
 
 Console.WriteLine("Введите три числа (a, b, c)"); //вывели значение в скобках
-Console.Write("Введите три числа: a");
+Console.Write("Введите три числа: a= ");
 int a=Convert.ToInt32 ( Console.ReadLine()); // производим конвертацию в int 32
-Console.Write("Введите три числа: b");
+Console.Write("Введите три числа: b= ");
 int b=Convert.ToInt32 ( Console.ReadLine());
-Console.Write("Введите три числа: c");
+Console.Write("Введите три числа: c= ");
 int c=Convert.ToInt32 ( Console.ReadLine());
 
 if (a<b) 
