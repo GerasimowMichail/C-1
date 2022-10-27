29eb1e7f8fb5833f253f30d22214d1f13bfa0f5d
Mischa <GerasimowMichail@yandex.ru>
2022-10-28 00:38:36 +0300

    решение задачи на поиск четных чисел из N

--- zad4/Program.cs
+++ zad4/Program.cs
@@ -5,11 +5,12 @@
 
 Console.WriteLine("Введите натуральное число ");
 int x=Convert.ToInt32 ( Console.ReadLine());
-
 int i = 1;
-while (i<x)
+
+while (i<=x)
+
 {
-   if (i%2 = 0)
-    Console.WriteLine("четные числа {i}");
+   if (i%2==0)
+    Console.Write( i);
     i=i+1;
 }
